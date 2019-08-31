6981c4c18e923a489796cf54595f2de1fcdf2bcc
joehe <leanbrown@live.cn>
2019-08-31 19:58:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# emb
+bigproject monitor
+
+## 更新
+### v1
+
+#### 1.0.0
+- [x] APP 保留消息
+- [x] Hardware 保留消息
+- [x] 性能测试
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  output: {
+    globalObject: 'this'
+  },
+  module: {
+    rules: [
+      {
+        test: [
+          /node_modules[/\\]mqtt[/\\]mqtt.js/,
+          /node_modules[/\\]mqtt[/\\]bin[/\\]sub.js/,
+          /node_modules[/\\]mqtt[/\\]bin[/\\]pub.js/,
+        ],
+        use: ['babel-loader', 'shebang-loader']
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      // 它会应用到普通的 `.js` 文件
+      // 以及 `.vue` 文件中的 `<script>` 块
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      // 它会应用到普通的 `.css` 文件
+      // 以及 `.vue` 文件中的 `<style>` 块
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
